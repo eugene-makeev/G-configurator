066dbe4f97a44c8243d2b4fb6e4e78c73a0fc66c
unknown <makeev.eugene@gmail.com>
2020-03-02 00:14:34 +0300

    minor changes

--- G-Configurator/Form1.Designer.cs
+++ G-Configurator/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.button3 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // button1
@@ -75,12 +77,26 @@
             // 
             // listView1
             // 
-            this.listView1.HideSelection = false;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listView1.GridLines = true;
+            this.listView1.LabelWrap = false;
             this.listView1.Location = new System.Drawing.Point(12, 50);
+            this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(539, 511);
             this.listView1.TabIndex = 9;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Parameter";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Value";
             // 
             // Form1
             // 
@@ -106,6 +122,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
 
